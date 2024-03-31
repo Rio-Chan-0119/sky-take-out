f7d7feee066b097e70580415cf4cb89899237b74
Rio-Chan-0119 <chenrh29@mail2.sysu.edu.cn>
2024-03-31 17:18:28 +0800

    实现新增员工模块

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.sky.controller.admin;
 
 import com.sky.constant.JwtClaimsConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
@@ -8,6 +9,7 @@ import com.sky.result.Result;
 import com.sky.service.EmployeeService;
 import com.sky.utils.JwtUtil;
 import com.sky.vo.EmployeeLoginVO;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.awt.geom.RectangularShape;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -37,6 +40,7 @@ public class EmployeeController {
      * @param employeeLoginDTO
      * @return
      */
+    @ApiOperation("员工登录")
     @PostMapping("/login")
     public Result<EmployeeLoginVO> login(@RequestBody EmployeeLoginDTO employeeLoginDTO) {
         log.info("员工登录：{}", employeeLoginDTO);
@@ -66,8 +70,22 @@ public class EmployeeController {
      *
      * @return
      */
+    @ApiOperation("员工登出")
     @PostMapping("/logout")
     public Result<String> logout() {
+        return Result.success();
+    }
+
+    /**
+     * 新增员工
+     * @param employeeDto
+     * @return
+     */
+    @PostMapping("")
+    @ApiOperation("新增员工")
+    public Result<String> save(@RequestBody EmployeeDTO employeeDto) {
+        log.info("新增员工：{}", employeeDto);
+        employeeService.save(employeeDto);
         return Result.success();
     }
 
--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -22,6 +25,21 @@ public class GlobalExceptionHandler {
     public Result exceptionHandler(BaseException ex){
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
+    }
+
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex) {
+        String msg = ex.getMessage();
+        if (msg.contains("Duplicate entry")) {
+            // 这段莫名其妙的 if 代码主要是根据用户名已存在时后台console抛出的异常信息
+            // 这个handler接收的异常对象的类型也是根据后台抛出的异常信息决定的
+            // username的解析方式，也得看后台！
+            String username = msg.split(" ")[2];
+            return Result.error("用户名" + username + MessageConstant.ALREADY_EXISTS);
+        } else {
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
+
     }
 
 }
